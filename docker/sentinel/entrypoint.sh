0238f6f95b01abbd27e2be9f213785827efa79ad
Sergey Dolin <sergey@s4y.solutions>
2018-02-26 23:43:23 +0500

    docker: seems to be no launch error

--- docker/sentinel/entrypoint.sh
+++ docker/sentinel/entrypoint.sh
@@ -3,5 +3,7 @@
 sed -i.bak "s/^port .*$/port ${PORT}/" /etc/redis/sentinel.conf
 sed -i.bak "s/^\(sentinel monitor [^ ]*\).*$/\1 ${REDIS_HOST} ${REDIS_PORT} ${QUORUM}/" /etc/redis/sentinel.conf
 rm /etc/redis/sentinel.conf.bak
-cat /etc/redis/sentinel.conf
 chown redis:redis /etc/redis/sentinel.conf
+cat /etc/redis/sentinel.conf
+echo $@
+exec $@
